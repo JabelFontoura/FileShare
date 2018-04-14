880a8e60002f0c354c1385dca991cc405192d152
JabelFontoura <jabel_fontoura@hotmail.com>
2018-04-14 14:08:56 -0300

    intial commit

---
+++ FileShare.Server/FileShare.Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileShare.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Server!");
+        }
+    }
+}
